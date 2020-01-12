0013ae1f3d63fe583555e594ec798547e51c247b
Choi Changhwan <ccwhan6@naver.com>
2020-01-13 01:50:10 +0900

    baekjoon 17825_not finished

--- Changhwan/baekjoon17825_yuut.cpp
+++ Changhwan/baekjoon17825_yuut.cpp
@@ -1,59 +1,95 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-struct map{
+struct map {
 	int index;
 	struct map* next;
-	int score;
 };
 int maxcost = 0;
 int d[10];
-int k[4] = { 0, };
-
+void travers(int k1, int k2, int k3, int k4, int dice, int i, int cost);
 struct map m[33];
 int score1[33] = { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 13, 16, 19, 22, 24, 28, 27, 26, 25, 30, 35 };
 int main(void) {
-	
+	for (int i = 0; i < 10; i++) {
+		cin >> d[i];
+	}
+
 	for (int i = 0; i < 33; i++) {
 		m[i].index = i;
-		m[i].score = score1[i];
 		m[i].next = NULL;
 	}
+	m[5].next = &m[22];
+	m[24].next = &m[30];
+	m[32].next = &m[20];
+	m[10].next = &m[25];
+	m[26].next = &m[30];
+	m[15].next = &m[27];
+
+	travers(0, 0, 0, 0, 0, 0, 0);
+	travers(0, 0, 0, 0, 0, 1, 0);
+	travers(0, 0, 0, 0, 0, 2, 0);
+	travers(0, 0, 0, 0, 0, 3, 0);
+
+	cout << maxcost;
 
 	return 1;
 }
 
-void travers(int k[], int dice, int i, int cost) {
-	struct map t;
-	if (dice <= 9) {
-		if (k[i] == 5) {
-			if (d[dice] <= 3) {
-				k[i] = 21 + d[dice];
-				cost += m[k[i]].score;
+void travers(int k1, int k2, int k3, int k4, int dice, int i, int cost) {
+	int k[4] = { k1, k2, k3, k4 };
+	int loc = k[i];
+	int done = 0;
+	struct map* m_temp;
+
+	int cnt = d[dice];
+	if (cnt <= 20 && m[loc].next == NULL) {
+		loc += cnt;
+		if (loc > 20) {
+			loc = 21;
+		}
+	}
+	else if (m[loc].next != NULL || cnt >= 22) {
+		for (cnt; cnt > 0; cnt--) {
+			if (m[loc].next != NULL) {
+				m_temp = m[loc].next;
+				loc = m_temp->index;
 			}
 			else {
-				k[i] = 30 + d[dice] - 4;
-				cost += m[k[i]].score;
+				loc++;
 			}
-		}
-		else if (k[i] == 10) {
-			if (d[dice] <= 2) {
-				k[i] = 24 + d[dice];
-				cost += m[k[i]].score;
+			if (loc == 20 && cnt > 1) {
+				loc = 21;
+				break;
 			}
-			else {
-				k[i] = 30 + d[dice] - 3;
-				cost += m[k[i]].score;
-			}
-
-		}
-		else if (k[i] == 15) {
-
-		}
-		else {
-
 		}
 	}
 
+	dice++;
+	if (dice < 10) {
+		k[i] = loc;
+		if (k[i] != 21) {
+			cost += score1[loc];
+		}
+
+		for (int i = 0; i < 4; i++) {
+			if (k[i] != 21) {
+				travers(k[0], k[1], k[2], k[3], dice, i, cost);
+			}
+		}
+
+		if (k[0] == 21 && k[1] == 21 && k[2] == 21 && k[3] == 21) {
+			if (cost >= maxcost) {
+				maxcost = cost;
+			}
+		}
+	}
+	else {
+		if (cost >= maxcost) {
+			maxcost = cost;
+		}
+	}
 }
+
 
